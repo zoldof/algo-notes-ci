d83e290b3feab561602e09d3699b099a57aec05a
zoldof <zoldof.game@gmail.com>
2025-12-19 20:09:41 +0900

    Update main_.js

--- main_.js
+++ main_.js
@@ -6,10 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const repo = "algo-notes";
   const params = new URLSearchParams(location.search);
   const fname = params.get('fname');
-  if (fname) {
-    //* Cache-Control *
-    //fetch(`/${repo}/${fname}/${fname}.txt`)
-    fetch(`/${repo}/${fname}/${fname}.txt?_=${Date.now()}`)
+  const pathParts = location.pathname.split('/').filter(Boolean); 
+  const currentDir = pathParts[pathParts.length - 1]; // 最後の要素が省略index.htmlなら最後がディレクトリ
+
+  if (currentDir === fname) {
+    console.log(`Skipping fetch: already in ${fname}/`);
+    return;
+  }
+  if (!fname) {
+    document.getElementById("content").textContent = "fname not specified";
+    return;
+  }
+  //* Cache-Control *
+  //fetch(`/${repo}/${fname}/${fname}.txt`)
+  fetch(`/${repo}/${fname}/${fname}.txt?_=${Date.now()}`)
     .then(res => {
       if (!res.ok) throw new Error("fetch failed");
       return res.text();
@@ -27,8 +37,4 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch(err => {
       document.getElementById("content").textContent = err.message;
     });
-  } else {
-    // 本番ページ用：ビルド済み HTML
-    //content.innerHTML = `<object type="text/html" data="./index.html" style="width:100%;height:100%"></object>`;
-  }
 });
