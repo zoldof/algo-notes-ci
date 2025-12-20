618da4f263104ae930f97544a74d8aed3ee9bd0a
zoldof <zoldof.game@gmail.com>
2025-12-20 18:37:59 +0900

    Update entry.js

--- entry.js
+++ entry.js
@@ -4,9 +4,6 @@ import { renderKatex } from "./katex.js";
 document.addEventListener("DOMContentLoaded", async () => {
   const el = document.getElementById("content");
   if (!el) return;
-
-  //if (!el.innerHTML.trim()) {
   await renderMarkdown(el);
-  //}
   renderKatex(el);
 });
