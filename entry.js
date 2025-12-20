11f3eb2375e35ef44d05c931934f373176c47f99
zoldof <zoldof.game@gmail.com>
2025-12-20 17:53:30 +0900

    Update entry.js

--- entry.js
+++ entry.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   const el = document.getElementById("content");
   if (!el) return;
 
-  await renderMarkdown(el);
+  if (!el.innerHTML.trim()) {
+    await renderMarkdown(el);
+  }
   renderKatex(el);
 });
