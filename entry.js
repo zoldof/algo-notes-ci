3a658d9358cf3252b9784cc67a2487efa0b2c144
zoldof <zoldof.game@gmail.com>
2025-12-21 14:47:32 +0900

    Update entry.js

--- entry.js
+++ entry.js
@@ -4,14 +4,6 @@ import { renderKatex } from "/algo-notes/katex.js";
 document.addEventListener("DOMContentLoaded", async () => {
   const el = document.getElementById("content");
   if (!el) return;
-  
-  await renderMarkdown(el);
-  renderKatex(el);
-});
-
-document.addEventListener("DOMContentLoaded", async () => {
-  const el = document.getElementById("content");
-  if (!el) return;
   await renderMarkdown(el);
   renderKatex(el);
 });
