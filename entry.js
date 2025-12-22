109ad9cca11bd9b550147e8314363b8b757ac2af
zoldof <zoldof.game@gmail.com>
2025-12-23 07:50:50 +0900

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,10 +1,10 @@
-import { renderKatex } from "/algo-notes/katex.js";
+import { renderKatex } from "/algo-notes-ci/katex.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const el = document.getElementById("content");
   if (!el) return;
   if (window.marked) {
-    const { renderMarkdown } = await import("/algo-notes/main.js");
+    const { renderMarkdown } = await import("/algo-notes-ci/main.js");
     await renderMarkdown(el);
   }
   renderKatex(el);
