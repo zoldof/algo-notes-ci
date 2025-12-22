fb3b4f98f6ba037257c9f49dc807319d2dda75b4
zoldof <zoldof.game@gmail.com>
2025-12-23 08:51:46 +0900

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,12 +1,12 @@
 const url = new URL(import.meta.url);
 const repo = url.pathname.split("/")[1];
-import { renderKatex } from `/${repo}/katex.js`;
+import { renderKatex } from `"/${repo}/katex.js"`;
 
 document.addEventListener("DOMContentLoaded", async () => {
   const el = document.getElementById("content");
   if (!el) return;
   if (window.marked) {
-    const { renderMarkdown } = await import(`/${repo}/main.js`);
+    const { renderMarkdown } = await import(`"/${repo}/main.js"`);
     await renderMarkdown(el);
   }
   renderKatex(el);
