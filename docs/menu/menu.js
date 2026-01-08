dccdf8cab5681395ba36a723378c882e75226a5a
zoldof <zoldof.game@gmail.com>
2026-01-08 10:14:08 +0900

    Create menu.js

---
+++ docs/menu/menu.js
@@ -0,0 +1,19 @@
+async function render() {
+  const el = document.getElementById("content");
+  if (!el) return;
+
+  try {
+    const res = await fetch(`menu/menu.md`);
+    if (!res.ok) throw new Error("fetch failed");
+
+    const text = await res.text();
+    marked.setOptions({
+      breaks: true,
+      gfm: true
+    });
+    el.innerHTML = marked.parse(text);
+  } catch (err) {
+    el.textContent = err.message;
+  }
+}
+document.addEventListener("DOMContentLoaded", render);
