fca245dba8f539f30f0ec926ab2ea78124e9bd95
zoldof <zoldof.game@gmail.com>
2026-01-10 20:30:22 +0900

    Update katex.js

--- docs/katex.js
+++ docs/katex.js
@@ -1,5 +1,5 @@
 export function renderKatex(el) {
-  window.renderMathInElement(el, {
+  renderMathInElement(el, {
     delimiters: [
       { left: "$$", right: "$$", display: true },
       { left: "$", right: "$", display: false }
