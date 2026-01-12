8c45bc5c93714d1f71061508bc8a2730028b97a7
zoldof <zoldof.game@gmail.com>
2026-01-12 11:15:46 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,22 +1,29 @@
+import { renderKatex } from "./katex.js";
+
 async function render() {
   const el = document.getElementById("content");
   if (!el) return;
   const params = new URLSearchParams(location.search);
   const dname = params.get("dname");
 
+  if (!dname) {
+    el.textContent = "dname not specified";
+    return;
+  }
+
   try {
     const res = await fetch(`dist/${dname}/index.md`);
     let text ="";
-    
+
     if (res.ok) {
       text = await res.text();
     }
     else if (res.status === 404) {
-      const markedRes = await fetch(`index.md`);
+      const markedRes = await fetch(`dist-marked/${dname}/index.md`);
       if (!markedRes.ok) {
         throw new Error("both fetches failed");
       }
-      text = await markedRes.text();
+      text = markedRes.text();
     }
     else {
       throw new Error(`fetch failed: ${res.status}`);
@@ -27,10 +34,7 @@ async function render() {
       gfm: true
     });
     el.innerHTML = marked.parse(text);
-    if (dname) {
-      const { renderKatex } = await import("./katex.js");
-      renderKatex(el);
-    }
+    renderKatex(el);
   } catch (err) {
     el.textContent = err.message;
   }
